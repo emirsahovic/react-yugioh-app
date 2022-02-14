290326fdbd4ba014d16af2e903494ca30d607dc7
emirsahovic <emir.sahovic00@gmail.com>
2022-02-14 12:30:24 +0100

    React Router v6 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import CardList from "./card/CardList";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import SpellcasterCard from "./card/SpellcasterCard";
+import DragonCard from "./card/DragonCard";
+import ZombieCard from "./card/ZombieCard";
+import MachineCard from "./card/MachineCard";
+
 function App() {
   return (
-    <div>
-
-    </div>
-  );
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CardList />} />
+        <Route path="/spellcaster" element={<SpellcasterCard />} />
+        <Route path="/dragon" element={<DragonCard />} />
+        <Route path="/zombie" element={<ZombieCard />} />
+        <Route path="/machine" element={<MachineCard />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
